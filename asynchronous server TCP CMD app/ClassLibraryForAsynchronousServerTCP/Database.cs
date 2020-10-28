a893cce90c30eeabae8a6696e994d4caf9a0bb19
Mateusz Kuźniak <mateokuz@gmail.com>
2020-10-28 09:30:54 +0100

    Konstruktor tworzący bazę danych wraz z tabelą 'users'. Funckja sprawdzająca czy połączenie z bazą danych jest otwarta

--- asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Database.cs
+++ asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Database.cs
@@ -11,16 +11,82 @@ namespace ClassLibraryForAsynchronousServerTCP
     public class Database
     {
         public SQLiteConnection myDatabaseConnection;
-        string databaseName = "database.userDatabase";
+        string databaseName = "database.users";
+        SQLiteCommand command;
 
+        /// <summary>
+        /// Sprawdza czy jest otwarte połączenie z bazą danych. Jeżeli nie to otwiera połączenie
+        /// </summary>
+        /// <param name="databaseConnection">połączenie SQLite</param>
+        void openConnection(SQLiteConnection databaseConnection)
+        {
+            if(databaseConnection != null && databaseConnection.State == System.Data.ConnectionState.Closed)
+            {
+                myDatabaseConnection.Open();
+            }
+        }
+
+        /// <summary>
+        /// Sprawdza czy tabela została utworzona
+        /// </summary>
+        /// <param name="tableName">nazwa tabeli do sprawdzenia</param>
+        /// <param name="databaseConnection"> połączenie SQLite </param>
+        /// <returns></returns>
+        bool checkForTableExist(string tableName, SQLiteConnection databaseConnection) 
+        {
+            bool exists;
+            openConnection(databaseConnection);
+
+            try
+            {
+                command = new SQLiteCommand (
+                    "select case when exists((select * from information_schema.tables where table_name = '" + tableName + "')) then 1 else 0 end", databaseConnection);
+                exists = (int)command.ExecuteScalar() == 1;
+            }
+            catch
+            {
+                try
+                {
+                    exists = true;
+                    SQLiteCommand cmdOther = new SQLiteCommand("SELECT 1 FROM" + tableName + " WHERE 1=0", databaseConnection);
+                    cmdOther.ExecuteNonQuery();
+                }
+                catch
+                {
+                    exists = false;
+                }
+            }
+
+            return exists;
+        }
+
+        /// <summary>
+        /// Konstruktor database. Jeżeli baza danych nie istnieje to ją tworzy
+        /// </summary>
         public Database()
         {
             myDatabaseConnection = new SQLiteConnection("Data Source=" + databaseName);
+            command = new SQLiteCommand(myDatabaseConnection);
 
             if(!File.Exists("./" + databaseName))
             { 
                 SQLiteConnection.CreateFile(databaseName);
                 Console.WriteLine("Database file created");
+
+                myDatabaseConnection.Open();
+
+                command.CommandText = @"CREATE TABLE users(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_name varchar(50) NOT NULL UNIQUE,
+                        password varchar(255) NOT NULL)";
+                command.ExecuteNonQuery();
+
+                if (checkForTableExist("users", myDatabaseConnection))
+                {
+                    Console.WriteLine("Tabela users została utworzona");
+                }
+                else
+                    Console.WriteLine("Tabela nie została utworzona");
             }
         }
 
