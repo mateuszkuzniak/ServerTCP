e7d29a0345783d5ca182a4aeea5ea59fadd28c8a
Mateusz Kuźniak <mateokuz@gmail.com>
2020-10-28 08:23:22 +0100

    Stworzenie konstruktora baz danych - utworzenie pliku bazydanych

--- asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Database.cs
+++ asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Database.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ClassLibraryForAsynchronousServerTCP
 {
-    public class Class1
+    public class Database
     {
+        public SQLiteConnection myDatabaseConnection;
+        string databaseName = "database.userDatabase";
+
+        public Database()
+        {
+            myDatabaseConnection = new SQLiteConnection("Data Source=" + databaseName);
+
+            if(!File.Exists("./" + databaseName))
+            { 
+                SQLiteConnection.CreateFile(databaseName);
+                Console.WriteLine("Database file created");
+            }
+        }
+
+
     }
 }
--- asynchronous server TCP CMD app/asynchronous server TCP CMD app/Program.cs
+++ asynchronous server TCP CMD app/asynchronous server TCP CMD app/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassLibraryForAsynchronousServerTCP;
 
 namespace asynchronous_server_TCP_CMD_app
 {
@@ -10,7 +11,7 @@ namespace asynchronous_server_TCP_CMD_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello word");
+            Database databaseObject = new Database();
             Console.ReadLine();
         }
     }
