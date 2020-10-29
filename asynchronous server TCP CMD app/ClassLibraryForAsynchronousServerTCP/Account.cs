57331d852c5844d2f524f7e3d6dfff3748a6c740
Mateusz Kuźniak <mateokuz@gmail.com>
2020-10-29 12:38:47 +0100

    Dodanie klasy pomocniczej Account

---
+++ asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryForAsynchronousServerTCP
+{
+    class Account
+    {
+        public int? id { get; set; }
+        public string login { get; set; }
+        public string pass { get; set; }
+    }
+}
