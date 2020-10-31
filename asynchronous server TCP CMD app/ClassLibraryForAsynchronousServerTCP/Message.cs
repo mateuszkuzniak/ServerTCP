ae7a428b745efc4858cf99fdc9807ca097e4c72e
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-01 00:33:50 +0100

    Dodanie statycznej klasy Message, która przechowuje wiadomości serwera

---
+++ asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibraryForAsynchronousServerTCP
+{
+
+    public static class Message { 
+    
+        public static readonly byte[] helloMessage = new ASCIIEncoding().GetBytes("Witaj! Zaloguj się do serwera. Podaj login:\n\r");
+    }
+}
