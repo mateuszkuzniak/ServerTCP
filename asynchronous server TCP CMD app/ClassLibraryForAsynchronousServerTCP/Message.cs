3b5bdc82d3f6c9f486330f8f5687d39775a6f2f1
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-01 02:11:45 +0100

    Doposażenie serwera w kolejne komunikaty

--- asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Message.cs
+++ asynchronous server TCP CMD app/ClassLibraryForAsynchronousServerTCP/Message.cs
@@ -10,6 +10,9 @@ namespace ClassLibraryForAsynchronousServerTCP
 
     public static class Message { 
     
-        public static readonly byte[] helloMessage = new ASCIIEncoding().GetBytes("Witaj! Zaloguj się do serwera. Podaj login:\n\r");
+        public static readonly byte[] helloMessage = new ASCIIEncoding().GetBytes("Hello!\n\r 1. Log in\n\r2. Register:\n\r");
+        public static readonly byte[] menuError= new ASCIIEncoding().GetBytes("ERROR: This option does not exist\n\r");
+        public static readonly byte[] invalidChar= new ASCIIEncoding().GetBytes("ERROR: Just enter the number\n\r");
+
     }
 }
