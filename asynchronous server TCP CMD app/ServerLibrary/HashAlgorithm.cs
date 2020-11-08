fbd62781e96bd5f8b99394a533f1fd8d5b5bed4b
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-08 16:56:53 +0100

    Dodanie funkcji skrótu

---
+++ asynchronous server TCP CMD app/ServerLibrary/HashAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace ServerLibrary
+{
+    public class HashAlgorithm
+    {
+        public static string getHash( string source)
+        {
+            var sBuilder = new StringBuilder();
+            using (SHA256 hashAlgorithm = SHA256.Create())
+            {
+                byte[] hashArray = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(source));
+
+                for (int i = 0; i < hashArray.Length; i++)
+                {
+                    sBuilder.Append(hashArray[i].ToString("x2"));
+                }
+            }
+            return sBuilder.ToString();
+        }
+
+    }
+}
