77d8385b7386732446a3bb9fe6c66d723be642d0
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-16 00:07:19 +0100

    abstrakcja na poziomie klas. Dodanie klasy obsługi plików

---
+++ asynchronous server TCP CMD app/DatabaseLibrary/DatabaseAbstract.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary
+{
+    public abstract class DatabaseAbstract
+    {
+        protected SQLiteCommand _command;
+        protected SQLiteConnection _myDatabaseConnection;
+        protected readonly object keyLock = new object();
+
+
+        /// <summary>
+        /// Konstruktor database. Jeżeli baza danych nie istnieje to ją tworzy
+        /// </summary
+
+        protected DatabaseAbstract(string databaseName)
+        {
+            _myDatabaseConnection = new SQLiteConnection($"Data Source=database.{databaseName}");
+            _command = new SQLiteCommand(_myDatabaseConnection);
+
+            if(!File.Exists($"./database.{databaseName}"))
+            {
+                SQLiteConnection.CreateFile(databaseName);
+                Console.WriteLine($"{databaseName} was created");
+            }
+        }
+
+        /// <summary>
+        /// Funkcja sprawdza czy jest otwarte połączenie z bazą danych. Jeżeli nie to otwiera połączenie
+        /// </summary>
+        /// <param name="databaseConnection">połączenie SQLite</param>
+        protected void openConnection()
+        {
+            if (_myDatabaseConnection != null && _myDatabaseConnection.State == System.Data.ConnectionState.Closed)
+            {
+                _myDatabaseConnection.Open();
+            }
+        }
+
+        /// <summary>
+        /// Funckja sprawdza czy jest zamknięte połączenie z bazą danych. Jeżeli nie to zamyka połączenie
+        /// </summary>
+        protected void cloceConnection()
+        {
+            if (_myDatabaseConnection != null && _myDatabaseConnection.State == System.Data.ConnectionState.Open)
+            {
+                _myDatabaseConnection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Sprawdza czy tabela została utworzona
+        /// </summary>
+        /// <param name="tableName">nazwa tabeli do sprawdzenia</param>
+        /// <param name="databaseConnection"> połączenie SQLite </param>
+        /// <returns></returns>
+        protected bool checkForTableExist(string tableName)
+        {
+            bool exists;
+            openConnection();
+
+
+                try
+                {
+                    _command.CommandText =
+                    $"select case when exists((select * from information_schema.tables where table_name = '{tableName}')) then 1 else 0 end";
+                    exists = (int)_command.ExecuteScalar() == 1;
+                }
+                catch
+                {
+                    try
+                    {
+                        exists = true;
+                        SQLiteCommand cmdOther = new SQLiteCommand($"SELECT 1 FROM {tableName} WHERE 1=0", _myDatabaseConnection);
+                        cmdOther.ExecuteNonQuery();
+                    }
+                    catch
+                    {
+                        exists = false;
+                    }
+                }
+
+            return exists;
+        }
+    }
+}
---
+++ asynchronous server TCP CMD app/DatabaseLibrary/DatabaseUser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseLibrary
+{
+    public class DatabaseUser : DatabaseAbstract
+    {
+        string _tableName;
+
+        public string TableName { get => _tableName; set => _tableName = value; }
+
+        public DatabaseUser(string databaseName, string tableName) : base(databaseName)
+        {
+
+            if (File.Exists("./database." + databaseName))
+            {
+                _myDatabaseConnection.Open();
+
+                if(!checkForTableExist(tableName))
+                {
+                    _command.CommandText = $@"CREATE TABLE {tableName}(
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        user_name varchar(50) NOT NULL UNIQUE,
+                        password varchar(255) NOT NULL,
+                        isLogged BOOLEAN DEFAULT '0')";
+                    _command.ExecuteNonQuery();
+
+                    if (checkForTableExist(tableName))
+                    {
+                        Console.WriteLine($"{tableName} table has been created");
+                    }
+                    else
+                        Console.WriteLine($"{tableName} table not created");
+                }
+                else
+                {
+                    openConnection();
+                    _command.CommandText = $"UPDATE {tableName} SET isLogged = '0' WHERE isLogged = '1'";
+                    _command.ExecuteNonQuery();
+                }
+
+                TableName = tableName;
+
+
+            }
+            else
+            {
+                openConnection();
+                _command.CommandText = $"UPDATE {databaseName} SET isLogged = '0' WHERE isLogged = '1'";
+                _command.ExecuteNonQuery();
+            }
+        }
+
+        public bool checkUserExist(string userName)
+        {
+            openConnection();
+            string userNameToLower = userName.ToLower();
+            lock(keyLock)
+            {
+                _command.CommandText = $"SELECT id FROM {TableName} WHERE user_name = '{userNameToLower}'";
+                if (_command.ExecuteScalar() != null)
+                    return true;
+                else
+                    return false;
+            }
+
+        }
+
+        /// <summary>
+        /// Funkcja dodająca użytkowników do bazy danych 
+        /// </summary>
+        /// <param name="name">nazwa użytkownika</param>
+        /// <param name="pass">hasło użytkownika</param>
+        public void addUser(string name, string pass)
+        {
+            openConnection();
+            if (checkForTableExist(TableName)) 
+            {
+                try
+                {
+                    lock(keyLock)
+                    {
+                        _command.CommandText = $"INSERT INTO {TableName} (user_name, password)" +
+                                             "VALUES('" + name + "','" + pass + "')";
+                        _command.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine($"{TableName} added successfully");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("The specified username is taken. Error: " + e.GetHashCode());
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Funkcja wyszukująca użytkownika w bazie danych z użyciem nazwy użytkownika
+        /// </summary>
+        /// <param name="user_name">nazwa użytkownika</param>
+        /// <returns></returns>
+        public Account getUserWithDatabase(string userName)
+        {
+            openConnection();
+            string userNameToLower = userName.ToLower();
+            Account user = new Account();
+
+            lock (keyLock)
+            {
+                _command.CommandText = $"SELECT * FROM {TableName} WHERE user_name = '{userNameToLower}'";
+                SQLiteDataReader reader = _command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    user.Id = reader.GetInt16(0);
+                    user.Login = reader.GetString(1);
+                    user.Pass = reader.GetString(2);
+                    user.IsLogged = reader.GetBoolean(3);
+                }
+
+
+                reader.Close();
+            }
+
+            return user;
+        }
+
+        
+        /// <summary>
+        /// Funkcja zmienia status użytkownika: zalogowany/wylogowany
+        /// </summary>
+        /// <param name="account">Konto</param>
+        public void updateLoginStatus(Account account)
+        {
+            openConnection();
+            lock(keyLock)
+            {
+                if (account.IsLogged)
+                {
+                    _command.CommandText = $"UPDATE {TableName} SET isLogged = '0' WHERE id='{account.Id}'";
+                    account.IsLogged = false;
+                }
+                else
+                {
+                    _command.CommandText = $"UPDATE {TableName} SET isLogged = '1' WHERE id='{account.Id}'";
+                    account.IsLogged = true;
+                }
+                _command.ExecuteNonQuery();
+            }
+        }
+    }
+}
