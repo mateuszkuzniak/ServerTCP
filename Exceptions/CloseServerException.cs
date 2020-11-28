956248e84f48d2a3572ece82473afeb270c63b4b
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-28 16:44:21 +0100

    Exceptions library

---
+++ Exceptions/CloseServerException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+
+namespace Exceptions
+{
+    public class CloseServerException : Exception
+    {
+        public CloseServerException() : base() { }
+        public CloseServerException(string message) : base(message) { }
+        public CloseServerException(string message, Exception inner) : base(message, inner) { }
+
+        protected CloseServerException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+    }
+}
