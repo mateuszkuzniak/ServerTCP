74f046542bb2a4aee5a1828da9e791be6a39f8e9
Mateusz Kuźniak <mateokuz@gmail.com>
2020-11-28 15:35:56 +0100

    library containing messages

---
+++ MessageLibrary/DbMessage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageLibrary
+{
+    public static class DbMessage
+    {
+        public static string invDbNameERROR = "Database name cannot be empty";
+        public static string invTableNameERROR = "Tablename cannot be empty";
+        public static string invFileListERROR = "File list is empty!";
+
+
+        public static string CreateTable(string tableName)
+        {
+            return $"{tableName} table has been created";
+        }
+
+        public static string CreateUser(string userName)
+        {
+            return $"User {userName} added successfully";
+        }
+    }
+}
