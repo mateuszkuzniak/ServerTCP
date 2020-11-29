ce87536707b610c72f78ce4177c53354daeb9fab
Jordan Kondracki <jordankondracki1999@gmail.com>
2020-11-29 20:58:12 +0100

    added unit tests

---
+++ UnitTests/ConnectionTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerLibrary;
+//using Xunit;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class ConnectionTests
+    {
+        [TestMethod]
+        public void IPAddressTest()
+        {
+            Assert.ThrowsException<FormatException>(() => new ServerTAP<LoginServerProtocol>(IPAddress.Parse("127."), 2048));
+        }
+        [TestMethod]
+        public void PortTest()
+        {
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => new ServerTAP<LoginServerProtocol>(IPAddress.Parse("127.0.0.1"), 80).Port = 80);
+        }
+    }
+}
