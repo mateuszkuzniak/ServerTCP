06b5d813b71603b20e04cea93645a6dd14f8a6de
Jordan Kondracki <jordankondracki1999@gmail.com>
2021-01-03 16:41:29 +0100

    added logs screen

---
+++ TCP Client/LogsScreen.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TCP_Client
+{
+    partial class LogsScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLogs = new System.Windows.Forms.RichTextBox();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonRefresh = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLogs
+            // 
+            this.textBoxLogs.Location = new System.Drawing.Point(3, 35);
+            this.textBoxLogs.Name = "textBoxLogs";
+            this.textBoxLogs.Size = new System.Drawing.Size(494, 462);
+            this.textBoxLogs.TabIndex = 0;
+            this.textBoxLogs.Text = "";
+            // 
+            // buttonBack
+            // 
+            this.buttonBack.Location = new System.Drawing.Point(3, 3);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(103, 26);
+            this.buttonBack.TabIndex = 1;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // buttonRefresh
+            // 
+            this.buttonRefresh.Location = new System.Drawing.Point(394, 3);
+            this.buttonRefresh.Name = "buttonRefresh";
+            this.buttonRefresh.Size = new System.Drawing.Size(103, 26);
+            this.buttonRefresh.TabIndex = 2;
+            this.buttonRefresh.Text = "Refresh";
+            this.buttonRefresh.UseVisualStyleBackColor = true;
+            this.buttonRefresh.Click += new System.EventHandler(this.buttonRefresh_Click);
+            // 
+            // LogsScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.buttonRefresh);
+            this.Controls.Add(this.buttonBack);
+            this.Controls.Add(this.textBoxLogs);
+            this.Name = "LogsScreen";
+            this.Size = new System.Drawing.Size(500, 500);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox textBoxLogs;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonRefresh;
+    }
+}
---
+++ TCP Client/LogsScreen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Client;
+
+namespace TCP_Client
+{
+    public partial class LogsScreen : UserControl
+    {
+        Form1 f = Form1.Instance;
+
+        public LogsScreen()
+        {
+            InitializeComponent();
+            Messages.sendMessage(Form1.Instance.connection, new string[] { "GETLOGS" });
+            string logs = Messages.receiveMessage(Form1.Instance.connection);
+            textBoxLogs.Text = logs;
+        }
+
+        private void buttonRefresh_Click(object sender, EventArgs e)
+        {
+            Messages.sendMessage(Form1.Instance.connection, new string[] { "GETLOGS" });
+            string logs = Messages.receiveMessage(Form1.Instance.connection);
+            textBoxLogs.Text = logs;
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            Form1.Instance.panel.Controls["Cloud"].BringToFront();
+        }
+    }
+}
