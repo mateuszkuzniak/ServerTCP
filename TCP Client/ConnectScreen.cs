d4151bc9af51b5eb361a3a124de8c0365045b6b5
Jordan Kondracki <jordankondracki1999@gmail.com>
2021-01-26 10:17:27 +0100

    Cleaning

--- TCP Client/ConnectScreen.cs
+++ TCP Client/ConnectScreen.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
 using System.Data;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Client;
-using System.Threading;
 using System.Net.NetworkInformation;
 
 namespace TCP_Client
--- TCP Client/LoginScreen.cs
+++ TCP Client/LoginScreen.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Client;
-using System.Net.Sockets;
 using Exceptions;
-using Client;
 
 namespace TCP_Client
 {
